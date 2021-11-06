3204baffddda380125588d0e8bbc1bd8d1faa572
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-11-06 19:10:31 +0100

    Implemented menu service

---
+++ src/Domain/Services/MenuService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Domain.Interfaces.Repositories;
+using Domain.Interfaces.Services;
+using Domain.Models;
+
+namespace Domain.Services
+{
+    public class MenuService : IMenuService
+    {
+        private readonly IMenuRepository _menuRepository;
+
+        public MenuService(IMenuRepository menuRepository)
+        {
+            _menuRepository = menuRepository;
+        }
+
+        public async Task<Menu> AddMenuItem(Menu menu, MenuItem menuItem)
+        {
+            if (menu.MenuItems.Contains(menuItem))
+                throw new Exception("Item already exists");
+            
+            if (!await _menuRepository.AddMenuItem(menu, menuItem))
+                throw new Exception("Something went wrong");
+
+            await _menuRepository.SaveChanges();
+            
+            menu.MenuItems.Add(menuItem);
+
+            return menu;
+        }
+    }
+}
