013fbc727279b25eb81c61d3118b151fe6c207cb
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-11-06 23:24:02 +0100

    Added db connection, user password rules and db seeding

--- src/Application/Startup.cs
+++ src/Application/Startup.cs
@@ -1,9 +1,14 @@
+using Domain.Models;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Serilog;
 
 namespace Application
 {
@@ -19,6 +24,28 @@ namespace Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDatabaseSeeder, DatabaseSeeder>();
+
+            services.AddDbContext<DemeterDbContext>(opt =>
+                {
+                    opt.LogTo(Log.Debug);
+                    opt.UseNpgsql(Configuration.GetConnectionString("DemeterConnection"));
+                }
+            );
+
+            services.AddIdentity<User, IdentityRole>(opt => opt.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<DemeterDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -31,6 +58,17 @@ namespace Application
         {
             if (env.IsDevelopment())
             {
+                var scopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
+                using (var scope = scopeFactory.CreateScope())
+                {
+                    var dbInitializer = scope.ServiceProvider.GetService<IDatabaseSeeder>();
+                    if (dbInitializer != null)
+                    {
+                        dbInitializer.Initialize();
+                        dbInitializer.Seed();
+                    }
+                }
+
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Application v1"));
