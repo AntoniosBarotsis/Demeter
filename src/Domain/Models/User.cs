a351f137e3dbaf09b848e45ea7c3e8298159e24c
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-11-08 17:36:45 +0100

    Changed orders to private and added equality checks

--- src/Domain/Models/User.cs
+++ src/Domain/Models/User.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KellermanSoftware.CompareNetObjects;
 
 namespace Domain.Models
 {
@@ -7,7 +10,7 @@ namespace Domain.Models
     /// </summary>
     public class User : UserAbstract
     {
-        public List<Order> PastOrders { get; set; }
+        private readonly CompareLogic _compareLogic = new();
 
         public User(string userName, string email)
         {
@@ -15,6 +18,35 @@ namespace Domain.Models
             Email = email;
             PastOrders = new List<Order>();
             UserType = UserType.User;
+        }
+
+        public List<Order> PastOrders { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && _compareLogic.Compare(this, obj).AreEqual;
+        }
+
+        private bool Equals(User other)
+        {
+            return _compareLogic.Compare(this, other).AreEqual && PastOrders.SequenceEqual(other.PastOrders);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_compareLogic, PastOrders);
+        }
+
+        public static bool operator ==(User left, User right)
+        {
+            return left is not null && left.Equals(right);
+        }
+
+        public static bool operator !=(User left, User right)
+        {
+            return left is not null && !left.Equals(right);
         }
     }
 }
