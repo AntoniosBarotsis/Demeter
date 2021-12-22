a5a0291d5003e47598ea0bedf7b4a1c1ac0ed24e
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-12-22 20:37:00 +0100

    Added user extension method

---
+++ src/Infrastructure/Data/MissingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public static class MissingExtensions
+    {
+        public static async Task<IQueryable<User>> BuildUser(this IQueryable<User> query, CancellationToken cancellationToken)
+        {
+            var tmp = query
+                .Include(u => u.PastOrders)
+                .ThenInclude(o => o.Items)
+                .AsSplitQuery()
+                .OrderBy(u => u.Id);
+            
+            await tmp.ForEachAsync(u =>
+            {
+                u.PastOrders.ForEach(order =>
+                {
+                    order.Price = order.Items.Sum(i => i.Price);
+                });
+            }, cancellationToken);
+
+            return tmp;
+        }
+    }
+}
